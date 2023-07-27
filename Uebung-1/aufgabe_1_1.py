d91f05648b90a63b8cced3a560b3f470df2be3b3
alexej-schelle <94290038+alexej-schelle@users.noreply.github.com>
2023-07-27 10:54:21 +0200

    Update aufgabe_1_1.py

--- Uebung-1/aufgabe_1_1.py
+++ Uebung-1/aufgabe_1_1.py
@@ -1,1 +1,16 @@
+# Von Robyn Eismann
 
+number = 2                          # 2 ist 1. Primzahl
+primeCounter = 1                    # counter start bei 1 wegen ersten Primzahl 2
+
+while primeCounter != 1000:     
+    number += 1                     # zählt die Zahlen
+    primeNumber = True            
+    for i in range (2,number):      # testet ob number eine Primzahl ist
+        if (number % i) == 0:       
+            primeNumber = False     # dann ist number keine Primzahl
+            break
+    if primeNumber == True:         
+        primeCounter += 1           # ist eine Primzahl, also imkrementieren wir den Counter  
+
+print (number)                      # gibt 1000. Primzahl raus
