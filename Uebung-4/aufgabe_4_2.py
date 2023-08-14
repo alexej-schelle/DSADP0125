c90ca5129f378958f857831fc7558c2093f70b03
alexej-schelle <94290038+alexej-schelle@users.noreply.github.com>
2023-08-14 10:33:57 +0200

    Update aufgabe_4_2.py

--- Uebung-4/aufgabe_4_2.py
+++ Uebung-4/aufgabe_4_2.py
@@ -1,1 +1,18 @@
+# Bearbeitet durch Jasper Sagawe
+ 
+alphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
 
+buchstabenWert = {buchstabe: nummer // 2 for nummer, buchstabe in enumerate(alphabet)}
+
+
+def stringZuWert(string):
+    wert = 0
+    for i in string:
+        if i in buchstabenWert:
+            wert += buchstabenWert[i]
+    return wert
+
+
+textstring = "Eine alte Dame geht heute einkaufen."
+
+print(stringZuWert(textstring))
