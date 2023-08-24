29d26880c4e9eea669712b1d687054db40c649de
alexej-schelle <94290038+alexej-schelle@users.noreply.github.com>
2023-08-24 17:49:50 +0200

    Add files via upload

---
+++ Uebung-5/seo-content.js
@@ -0,0 +1,513 @@
+<meta name = "title" content = "Your Company Name">  
+<meta name = "keywords" content = "..."/>
+<meta name = "description" content = "..."/>
+<meta name = "subject" content = "...">
+<meta name = "copyright"content = "...">
+<meta name = "language" content = "...">
+<meta name = "robots" content = "..." />
+<meta name = "revised" content = "..." />
+<meta name = "abstract" content = "...">
+<meta name = "topic" content = "...">
+<meta name = "summary" content = "...">
+<meta name = "Classification" content = "...">
+<meta name = "author" content = "...">
+<meta name = "designer" content = "...">
+<meta name = "copyright" content = "...">
+<meta name = "reply-to" content = "...">
+<meta name = "owner" content = "...">
+<meta name = "url" content = "...">
+<meta name = "identifier-URL" content = "...">
+<meta name = "category" content = "...">
+<meta name = "coverage" content = "...">
+<meta name = "distribution" content = "...">
+<meta name = "rating" content = "...">
+<meta name = "revisit-after" content = "...">
+<meta http-equiv = "Expires" content = "...">
+<meta http-equiv = "Pragma" content = "...">
+<meta name = "viewport" content = "width = ...">
+
+// Geschäftsangaben :
+
+<script type="application/ld+json"> {
+    "@context":"http://schema.org",
+    "@type":"Organization",
+    "address": {
+        "@type": "PostalAddress", 
+        "addressLocality": "...", 
+        "addressRegion": "...", 
+        "postalCode": "...", 
+        "streetAddress": "..."
+    }
+    ,
+    "name":"Your Company Name",
+    "url":"yourdomain",
+    "email":"...",
+    "telephone":"...",
+    "potentialAction": {
+        "@type":"ViewAction",
+        "target":["yourdomain",
+        {
+            "@type":"EntryPoint",
+            "urlTemplate":"yourdomain",
+            "actionApplication": {
+                "@type": "...", 
+                "name": "...", 
+                "operatingSystem": "..."
+            }
+        }
+        ,
+        "yourdomain/",
+        {
+            "@type":"EntryPoint",
+            "urlTemplate":"yourdomain",
+            "actionApplication": {
+                "@type": "SoftwareApplication", 
+                "name": "...", 
+                "operatingSystem": "..."
+            }
+        }
+        ,
+        "yourdomain/",
+        {
+            "@type":"EntryPoint",
+            "urlTemplate":"yourdomain",
+            "actionApplication": {
+                "@type": "SoftwareApplication", 
+                "name": "...", 
+                "operatingSystem": "..."
+            }
+        }
+        ]
+    }
+    ,
+    "exifData":[ {
+        "@type": "PropertyValue", 
+        "name": "...", 
+        "value": "..."
+    }
+    ,
+    "exifData":[ {
+        "@type": "PropertyValue", 
+        "name": "...", 
+        "value": "..."
+    }
+    ,
+    "exifData":[ {
+        "@type": "PropertyValue", 
+        "name": "...", 
+        "value": "..."
+    }
+    ,
+    "exifData":[ {
+        "@type": "PropertyValue", 
+        "name": "...", 
+        "value": "..."
+    }
+    ,
+    "exifData":[ {
+        "@type": "PropertyValue", 
+        "name": "...", 
+        "value": "..."
+    }
+}
+
+</script>
+
+// Geschäftszweige:
+
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [{
+    "@type": "ListItem",
+    "position": 1,
+    "name": "...",
+    "item": "..."
+  },{
+    "@type": "ListItem",
+    "position": 2,
+    "name": "...",
+    "item": "..."
+  },{
+    "@type": "ListItem",
+    "position": 3,
+    "name": "...",
+    "item": "..."
+  },{
+    "@type": "ListItem",
+    "position": 4,
+    "name": "...",
+    "item": "..."
+  }]
+}
+</script>
+
+// Geschäftsführer :
+
+    <script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "Organization",
+  "url": "",
+  "logo": "",
+  "contactPoint": [{
+    "@type": "ContactPoint",
+    "telephone": "...",
+    "contactType": "..."
+  }]
+}
+</script>
+ 
+ // Logo :
+      
+      {
+  "@context": "http://schema.org",
+  "@type": "Organization",
+  "url": "...,
+  "logo": "..."
+}
+
+// Social Profile :
+
+
+            <script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "Person",
+  "name": "your name",
+  "url": "",
+  "relatedlink": [
+    "http://www.facebook.com/your-profile",
+    "http://instagram.com/yourProfile",
+    "http://www.linkedin.com/in/yourprofile",
+    "http://plus.google.com/your_profile"
+  ]
+}
+</script>
+
+// Carousell of most important objects :
+
+{
+  "@type": "ListItem",
+  "position": 1,
+  "item": {
+    "@type": "Recipe",
+    "url": "...",
+    "name": "...",
+    "image": "...",
+    "aggregateRating": {
+      "@type": "AggregateRating",
+      "ratingValue": "...",
+      "reviewCount": "..."
+    },
+    "recipeYield": "...",
+    "recipeIngredient": [
+      "...",
+      "...",
+      "...",
+      "...",
+      "...",
+      "...",
+      "..."
+    ],
+  }
+}
+
+
+// Rating :
+
+<script type="application/ld+json"> {
+  "@context" : "http://schema.org/",
+  "@type": "EmployerAggregateRating",
+  "itemReviewed": {
+    "@type": "Organization",
+    "name" : "...",
+    "relatedlink" : "..."
+  },
+  "ratingValue": "...",
+  "bestRating": "...",
+  "worstRating": "...",
+  "ratingCount" : "..."
+}
+</script>
+
+
+// Veranstaltungen :
+
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "Event",
+  "name": "...",
+  "startDate": "...",
+  "location": {
+    "@type": "...",
+    "name": "...",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "...",
+      "addressLocality": "...",
+      "postalCode": "...",
+      "addressRegion": "",
+      "addressCountry": ""
+    }
+  },
+  "image": [
+    "...",
+    "...",
+    "..."
+   ],
+  "description": "...",
+  "endDate": "...",
+  "offers": {
+    "@type": "Offer",
+    "url": "...",
+    "price": "...",
+    "priceCurrency": "",
+    "availability": "...",
+    "validFrom": "..."
+  },
+  "performer": {
+    "@type": "PerformingGroup",
+    "name": "..."
+  }
+}
+</script>
+
+// Gastronomie oder Kino oder Hotel oder weitere :
+
+          <script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "...",
+  "@id": "...",
+  "name": "...",
+  "image": "...",
+  "relatedlink": "...",
+  "servesCuisine": "...",
+  "priceRange": "...",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "...",
+    "addressLocality": "...",
+    "addressRegion": "...",
+    "postalCode": "...",
+    "addressCountry": "..."
+  },
+  "telephone": "...",
+  "geo": {
+    "@type": "...",
+    "latitude": ...,
+    "longitude": ...
+  },
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "...",
+    "bestRating": "...",
+    "worstRating": "...",
+    "ratingCount": "..."
+  },
+  "review": {
+    "@type": "Review",
+    "url": "...",
+    "author": {
+      "@type": "Person",
+      "name": "...",
+      "relatedlink": "..."
+    },
+    "publisher": {
+      "@type": "Organization",
+      "name": "",
+      "relatedlink": "..."
+    },
+    "datePublished": "...",
+    "description": "...",
+    "inLanguage": "en",
+    "reviewRating": {
+      "@type": "Rating",
+      "worstRating": ...,
+      "bestRating": ...,
+      "ratingValue": ...
+    }
+  }
+}
+</script>
+
+
+// Öffnungszeiten :
+
+
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "Restaurant",
+  "image": [
+    "...",
+    "...",
+    "..."
+   ],
+  "@id": "...",
+  "name": "",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "...",
+    "addressLocality": "...",
+    "addressRegion": "...",
+    "postalCode": "...",
+    "addressCountry": "..."
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": ...,
+    "longitude": ...
+  },
+  "url": "...",
+  "telephone": "...",
+  "openingHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "...",
+        "..."
+      ],
+      "opens": "...",
+      "closes": "..."
+    },
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "...",
+        "...",
+        "..."
+      ],
+      "opens": "...",
+      "closes": "..."
+    },
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": "...",
+      "opens": "...",
+      "closes": "..."
+    },
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": "...",
+      "opens": "...",
+      "closes": "...0"
+    }
+  ],
+  "menu": "http://www.example.com/menu",
+  "acceptsReservations": "True"
+}
+</script>
+
+
+// Angaben zur Produktpalette:
+
+    <script type="application/ld+json">
+{
+  "@context": "http://schema.org/",
+  "@type": "Product",
+  "name": "",
+  "image": [
+    "...",
+    "...",
+    "..."
+   ],
+  "brand": {
+    "@type": "...",
+    "name": "..."
+  },
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "...",
+    "ratingCount": "..."
+  },
+  "offers": {
+    "@type": "AggregateOffer",
+    "lowPrice": "...",
+    "highPrice": "...",
+    "priceCurrency": "..."
+  }
+}
+</script>
+     
+     
+     
+// Videos :
+
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "VideoObject",
+  "name": "...",
+  "description": "...",
+  "thumbnailUrl": "...",
+  "uploadDate": "...",
+  "duration": "",
+  "publisher": {
+    "@type": "Organization",
+    "name": "",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "...",
+      "width": ...,
+      "height": ...
+    }
+  },
+  "contentUrl": "...",
+  "embedUrl": "...",
+  "interactionCount": "..."
+}
+</script>
+
+// Here comes main new part developed by krealix.de (Recommended : minimal 10 to maximal 100 different canonical keywords): 
+
+// INDEX STRUCTURE for NETWORK MAPPING. Note : replace relatedLink by sameAs, if source and aim are partners.
+
+// Partner 1 with keyword "1" -->
+
+<script type="application/ld+json">
+{ 
+  "@context": "http://schema.org", 
+  "@type": "WebSite", 
+  "url": "http://getbootstrap.com/", 
+  "name": "Bootstrap",
+   "author": {
+      "@type": "...",
+      "name": "..."
+    },
+  "description": "...",
+  "publisher": "Bootstrap",
+  "potentialAction": { 
+    "@type": "SearchAction", 
+    "target": "http://.../?s={keyword}", 
+    "query-input": "required name=keyword" }, 
+    "relatedlink" : "yourdomain/" 
+    }
+</script>
+
+// Fitting Partner 1 with keyword "1" <--
+
+<script type="application/ld+json">
+{ 
+  "@context": "http://schema.org", 
+  "@type": "WebSite", 
+  "url": "...", 
+  "name": "Your Company Name",
+   "author": {
+      "@type": "Person",
+      "name": "..."
+    },
+  "description": "...",
+  "publisher": "publisher name",
+  "potentialAction": { 
+    "@type": "SearchAction", 
+    "target": "yourdomain/?s={keyword}", 
+    "query-input": "required name=keyword" }, 
+   "relatedlink" : "http://..." 
+    }
+</script>
+
+// And so forth with different canonical keywords...
